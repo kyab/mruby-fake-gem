cef65f0ae1b123775c4474319586ace09a8a0155
kyab <kyossi212@gmail.com>
2013-07-04 20:02:00 +0900

    1st Fix

--- src/mruby-fake-gem.c
+++ src/mruby-fake-gem.c
@@ -7,7 +7,7 @@
 void
 mrb_mruby_fake_gem_gem_init(mrb_state* mrb) {
 
-  RClass *fakeClass = mrb_define_class(mrb, "Fake", mrb->object_class);
+  struct RClass *fakeClass = mrb_define_class(mrb, "Fake", mrb->object_class);
 
 }
 void
